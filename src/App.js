b4eb3c570376ee6fff956d375c0deadd844b7070
Siddhartha <sidduinevitable@gmail.com>
2021-06-12 08:19:56 +0530

    basic redux application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { Counter } from "./Counter";
-import "./styles.css";
+import { useSelector } from 'react-redux';
+import { Counter } from './Counter';
+import './styles.css';
 
 export default function App() {
+  const counter = useSelector((state) => state.counter.count);
   return (
     <div className="App">
       <h1>Basic React Redux application</h1>
-      <h2>Your total votes is: </h2>
+      <h2>Your total votes is: {counter}</h2>
+      <Counter />
+      <Counter />
       <Counter />
     </div>
   );
--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,17 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { increment, decrement } from './store/actions';
 
 export const Counter = () => {
   const [counter, updateCounter] = useState(0);
+  const dispatch = useDispatch();
   const handleDecrement = () => {
     updateCounter(counter - 1);
+    dispatch(decrement());
   };
   const handleIncrement = () => {
     updateCounter(counter + 1);
+    dispatch(increment());
   };
   return (
     <div>
