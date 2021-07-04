0beb0a2dfaf84fee80ad1a7cc6f5882ae190139d
siddharthasarma <sidduinevitable@gmail.com>
2021-07-04 18:03:39 +0530

    basic setup to make tailwind css work with react

---
+++ react-with-tailwind/src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div className="flex-shrink-0">
+          <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div classname="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-gray-500">You have a new message!</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
